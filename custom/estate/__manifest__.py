5ea7995f127ef7e83b994e1918134503e8eff493
Bonnak <c.bonnak@khmerdev.com>
2022-01-06 16:54:16 +0700

    Start views

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
     ]
 }
