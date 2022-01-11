a65d75599b0bc5291664fed6992720161a228130
Bonnak <c.bonnak@khmerdev.com>
2022-01-11 11:26:43 +0700

    Custom view

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -1,17 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyModel(models.Model):
-    _name = "estate.property"
-    _description = "Estate property"
+    _name = 'estate.property'
+    _description = 'Estate property'
 
-    name = fields.Char(required=True)
+    name = fields.Char('Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availibility = fields.Date()
+    date_availibility = fields.Date('Available From', copy=False,default=lambda self: fields.Datetime.today())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    selling_price = fields.Float(readonly=True,copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
@@ -22,3 +22,9 @@ class EstatePropertyModel(models.Model):
         ('East', 'East'),
         ('West','West'),
     ])
+    # total_area = fields.Float(compute='_compute_total_area')
+
+    # @api.depends('living_area', 'garden_area')
+    # def _compute_total_area(self):
+    #     for record in self:
+    #         record.total_area = 2.0 * record.living_area
